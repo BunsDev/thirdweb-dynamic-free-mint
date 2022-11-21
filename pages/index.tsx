3549e26850e6e96b8019d250890ebb7b786d93f2
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-11-21 20:51:06 +0100

    Add dynamic free mint code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,77 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import {
+  ConnectWallet,
+  useAddress,
+  useContract,
+  useContractRead,
+  Web3Button,
+} from '@thirdweb-dev/react';
+import { utils } from 'ethers';
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  const contractAddress = '0xfe088Bae1F873E9169E4668dD6D3e1c07DD9bDe1';
+  const address = useAddress();
+  const [quantity, setQuantity] = useState('1');
+  const { contract } = useContract(contractAddress);
+  const { data: price, isLoading } = useContractRead(
+    contract,
+    'priceForAddress',
+    address,
+    quantity,
+  );
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
+          Welcome to{' '}
+          <a href="https://thirdweb.com/nach.eth/DynamicFreeMint/">
+            Dynamic Free Mint
+          </a>
+          !
         </h1>
 
         <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
+          Limit how many NFTs each wallet can claim for free.
         </p>
 
         <div className={styles.connect}>
           <ConnectWallet />
         </div>
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
+        <div>
+          <label>
+            Quantity:
+            <input
+              type="number"
+              value={quantity}
+              min="1"
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+          </label>
+        </div>
 
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
+        <div style={{ marginTop: '10px' }}>
+          <Web3Button
+            contractAddress={contractAddress}
+            action={(contract) => {
+              contract.call('claim', address, quantity, {
+                value: price,
+              });
+            }}
+            isDisabled={!quantity || parseInt(quantity) < 1 || isLoading}
           >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+            Mint{' '}
+            {price
+              ? `(${
+                  price?.toString() === '0'
+                    ? 'Free'
+                    : `${utils.formatEther(price)} ETH`
+                })`
+              : ''}
+          </Web3Button>
         </div>
       </main>
     </div>
